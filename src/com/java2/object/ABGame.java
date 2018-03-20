e15719061331ff3d7914995560d459897efc071a
felix19990617 <felix3157538@gmail.com>
2018-03-20 16:24:17 +0800

    work

---
+++ src/com/java2/object/ABGame.java
@@ -0,0 +1,34 @@
+package com.java2.object;
+
+import java.util.Scanner;
+
+public class ABGame {
+
+	public static void main(String[] args) {
+		String secret = "9873";
+		int a = 0;
+		int b = 0;
+		Scanner scanner = new Scanner(System.in);
+		while (a != 4) {
+			a = 0;
+			b = 0;
+			System.out.print("Please enter your guess:");
+			String nums = scanner.nextLine();
+			int length = secret.length();
+			for (int i = 0; i < nums.length(); i++) {
+				char c = nums.charAt(i);
+				for (int j = 0; j < length; j++) {
+					if (c == secret.charAt(j)) {
+						if (i == j) {
+							a++;
+						} else {
+							b++;
+						}
+						break;
+					}
+				}
+			}
+			System.out.println(a + "A" + b + "B");
+		}
+	}
+}
---
+++ src/com/java2/object/ABnumber.java
@@ -0,0 +1,14 @@
+package com.java2.object;
+
+public class ABnumber {
+	String number;
+
+	public ABNumber(String number){
+			this.number = number;
+		}
+		public int validate(String secret){
+		
+
+	}
+
+}
